62c1e1b84f559eec5fe2dc8e9f29e6ea362e1875
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-03-30 23:17:48 -0600

    Escanner

---
+++ app/src/main/java/com/example/evenementerapp/CameraScanner.kt
@@ -0,0 +1,64 @@
+package com.example.evenementerapp
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import org.json.JSONException
+import org.json.JSONObject
+
+// TODO: Rename parameter arguments, choose names that match
+
+class CameraScanner : Fragment() {
+
+    internal var txtName: TextView? = null
+
+    internal var txtSiteName: TextView? = null
+
+    internal var btnScan: Button? = null
+    internal var qrScanIntegrator: IntentIntegrator? = null
+
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_camera_scanner, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        btnScan = view.findViewById(R.id.btnScan)
+        btnScan!!.setOnClickListener { performAction() }
+
+        qrScanIntegrator = IntentIntegrator.forSupportFragment(this)
+        qrScanIntegrator?.setOrientationLocked(false)
+
+
+    }
+
+    private fun performAction() {
+        qrScanIntegrator?.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(activity, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(activity, "Texto Escaneado: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
