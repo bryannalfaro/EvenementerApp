be191af7b49fa12f31b529a806db06cd73d2c231
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-03-30 22:09:24 -0600

    About us, Registro y vista de crear evento agregada

--- app/src/main/java/com/example/evenementerapp/Login.kt
+++ app/src/main/java/com/example/evenementerapp/Login.kt
@@ -9,6 +9,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 
 class Login : Fragment() {
 
@@ -27,6 +29,7 @@ class Login : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val button=view!!.findViewById<Button>(R.id.buttonSign)
+        val text=view.findViewById<TextView>(R.id.textView8)
 
         button.setOnClickListener {
 
@@ -34,6 +37,9 @@ class Login : Fragment() {
 
             startActivity(intent)
 
+        }
+        text.setOnClickListener {
+            findNavController().navigate(R.id.action_login2_to_register)
         }
     }
 }
--- app/src/main/java/com/example/evenementerapp/Register.kt
+++ app/src/main/java/com/example/evenementerapp/Register.kt
@@ -1,12 +1,16 @@
 package com.example.evenementerapp
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 
 
 class Register : Fragment() {
@@ -19,6 +23,25 @@ class Register : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+
+
         return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+        val button=view!!.findViewById<Button>(R.id.signbutton)
+
+
+        button.setOnClickListener {
+
+            val intent = Intent(activity, Main2Activity::class.java)
+
+            startActivity(intent)
+
+        }
+
     }
 }
